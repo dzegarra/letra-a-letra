5a1403f86c558a6b281da92b74104e9b09db8444
Daniel Zegarra <dzegarra@gmail.com>
2025-01-29 23:07:40 +0000

    Fix TS issue in the Circle component

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,34 +1,29 @@
-import { ComponentProps } from 'react';
-import clsx from 'clsx';
-import classes from './Circle.module.css';
+import { ComponentProps, CSSProperties } from "react";
+import clsx from "clsx";
+import classes from "./Circle.module.css";
 
 type CircleProps = {
   size: string;
   color1: string;
   color2: string;
   color3: string;
-} & ComponentProps<'div'>;
+} & ComponentProps<"div">;
 
-export const Circle = ({
-  size,
-  className,
-  color1,
-  color2,
-  color3,
-  ...props
-}: CircleProps) => (
+export const Circle = ({ size, className, color1, color2, color3, ...props }: CircleProps) => (
   <div
-    style={{
-      width: size,
-      height: size,
-      '--color1': color1,
-      '--color2': color2,
-      '--color3': color3,
-    }}
+    style={
+      {
+        width: size,
+        height: size,
+        "--color1": color1,
+        "--color2": color2,
+        "--color3": color3,
+      } as CSSProperties
+    }
     className={clsx(
       `top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 absolute rounded-full border border-slate-500`,
       classes.bgcolor,
-      className
+      className,
     )}
     {...props}
   ></div>
